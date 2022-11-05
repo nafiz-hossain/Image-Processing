e0ebcd14f2e500c9fbe5883012dd438d45e471f9
nafizzzzzzzzzz <nhremon8181@gmail.com>
2022-11-06 00:05:32 +0600

    Implemented method to improve a colored image by median filtering

--- medianFiltering.py
+++ medianFiltering.py
@@ -3,7 +3,7 @@ import numpy as np
 import statistics 
 
 
-# enum  	cv::ImreadModes {
+# enum cv::ImreadModes {
 #   cv::IMREAD_UNCHANGED = -1,
 #   cv::IMREAD_GRAYSCALE = 0,
 #   cv::IMREAD_COLOR = 1,
@@ -20,40 +20,174 @@ import statistics
 # }
 
 
+# (3, 4)
+# m=  3
+# n=  4
+# Input Image [[74 83 83 84]
+#              [82 92 88 80]
+#              [86 92 88 75]]
+# New Image [[ 0.  0.  0.  0.]
+#  [ 0. 86. 84.  0.]
+#  [ 0.  0.  0.  0.]]
+
+
+
+
+# b=  [[63 69 76 68]
+#  [71 70 73 65]
+#  [72 77 67 56]]
+# g=  [[75 80 77 78]
+#  [82 92 85 76]
+#  [84 89 85 75]]
+# r=  [[ 79  96  99 105]
+#  [ 89 103 101  94]
+#  [ 97 106 102  85]]
+# Input Image [[[ 63  75  79]
+#   [ 69  80  96]
+#   [ 76  77  99]
+#   [ 68  78 105]]
+
+#  [[ 71  82  89]
+#   [ 70  92 103]
+#   [ 73  85 101]
+#   [ 65  76  94]]
+
+#  [[ 72  84  97]
+#   [ 77  89 106]
+#   [ 67  85 102]
+#   [ 56  75  85]]]
+
+
+
+#The shape of an image is accessed by img.shape. It returns a tuple of the number of rows, columns, and channels (if the image is color): 
+#Channels refer to the number of colors. For example, there are three channels in a RGB image, the Red Channel, the Green Channel and the Blue Channel. Each of the channels in each pixel represents the intensity of each color that constitute that pixel.
 
 
 #take input by cv2
-img_input = cv2.imread('sample.png', 0)
-m, n = img_input.shape
+img_input = cv2.imread('sample.png', -1)
+m, n, c = img_input.shape
 
-print(img_input.shape)
+print('input image shape', img_input.shape)
 
+#m, n = 10
 
 #take a same size matrix set all value into zero
-img_new = np.zeros([m, n])
+# img_new = np.zeros([m, n, c])
 #print(img_new)
 
+print('m= ', m)
+print('n= ', n)
+print('c= ', c)
 
-print(img_input[0,0])
-for i in range(1, m-1):
-    for j in range(1, n-1):
-        temp = [ img_input[i-1, j-1],
-        img_input[i, j-1],
-        img_input[i+1, j-1],
-        img_input[i-1, j],
-        img_input[i, j],
-        img_input[i+1, j],
-        img_input[i-1, j+1],
-        img_input[i, j+1],
-        img_input[i+1, j+1],
+#img_new_blue = np.zeros([m+2, n+2])
+updatedImage = np.array([[[0]*3]*(n+2)]*(m+2))
+print('updatedImage shape', updatedImage.shape)
+print('updatedImage after declaring np array', updatedImage)
+
+
+updatedImage[1:m+1 , 1:n+1, :] = img_input
+
+print('updatedImage after using img_input', updatedImage)
+b, g, r = cv2.split(updatedImage)
+
+print('#b', b)
+
+
+
+b_new = np.zeros([m+2, n+2])
+g_new = np.zeros([m+2, n+2])
+r_new = np.zeros([m+2, n+2])
+
+
+print('b_new', b_new)
+
+for i in range(1, m+1):
+    for j in range(1, n+1):
+        tempForBlue = [ b[i-1, j-1],
+        b[i-1, j],
+        b[i-1, j+1],
+        b[i, j-1],
+        b[i, j],
+        b[i, j+1],
+        b[i+1, j-1],
+        b[i+1, j],
+        b[i+1, j+1],
         ]
+        tempForBlue = statistics.median(tempForBlue)
+        b_new[i, j]= tempForBlue
 
-        temp = statistics.median(temp)
-        img_new[i, j]= temp
- 
+        #print('b_new', b_new)
 #astype returns a new DataFrame where the data types has been changed to the specified type
 #numpy.uint8: 8-bit unsigned integer (0 to 255).
 
-img_new1 = img_new.astype(np.uint8)
-cv2.imwrite('filtered.png', img_new1)
+b_new = b_new.astype(np.uint8)
+# cv2.imwrite('filtered.png', img_new)
+
+#print('New Image', b_new)
+
+
+
+
+
+for i in range(1, m+1):
+    for j in range(1, n+1):
+        tempForGreen = [ g[i-1, j-1],
+        g[i-1, j],
+        g[i-1, j+1],
+        g[i, j-1],
+        g[i, j],
+        g[i, j+1],
+        g[i+1, j-1],
+        g[i+1, j],
+        g[i+1, j+1],
+        ]
+        tempForGreen = statistics.median(tempForGreen)
+        g_new[i, j]= tempForGreen
+
+        #print('g_neww', g_new)
+#astype returns a new DataFrame where the data types has been changed to the specified type
+#numpy.uint8: 8-bit unsigned integer (0 to 255).
+
+g_new = g_new.astype(np.uint8)
+# cv2.imwrite('filtered.png', img_new)
+
+#print('New Image', g_new)
+
+for i in range(1, m+1):
+    for j in range(1, n+1):
+        tempForRed = [ r[i-1, j-1],
+        r[i-1, j],
+        r[i-1, j+1],
+        r[i, j-1],
+        r[i, j],
+        r[i, j+1],
+        r[i+1, j-1],
+        r[i+1, j],
+        r[i+1, j+1],
+        ]
+        tempForRed = statistics.median(tempForRed)
+        r_new[i, j]= tempForRed
+
+
+
+        #print('r_neww', r_new)
+#astype returns a new DataFrame where the data types has been changed to the specified type
+#numpy.uint8: 8-bit unsigned integer (0 to 255).
+
+r_new = r_new.astype(np.uint8)
+# cv2.imwrite('filtered.png', imr_new)
+
+#print('New Image', r_new)
+b_final= b_new[1:m+1 , 1:n+1]
+g_final= g_new[1:m+1 , 1:n+1]
+r_final= r_new[1:m+1 , 1:n+1]
+
+final_img = cv2.merge((b_final,g_final,r_final))
+cv2.imwrite('filtered.png', final_img)
+print('Updated Image to', final_img)
+print('b_new should be', b_final)
+print('g_new should be', g_final)
+print('r_new should be', r_final)
+
+
 
