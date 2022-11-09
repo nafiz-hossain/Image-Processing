cf9fbcaaca56329363669c04a1f010d36dbbf4b3
nafizzzzzzzzzz <nhremon8181@gmail.com>
2022-11-10 04:49:13 +0600

    Deleted unused file

--- medianFiltering.py
+++ medianFiltering.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 import statistics
 import os
-from copy import deepcopy
-
+import csv
+import time
+import pandas as pd    
 # enum cv::ImreadModes {
 #   cv::IMREAD_UNCHANGED = -1,
 #   cv::IMREAD_GRAYSCALE = 0,
@@ -54,23 +55,28 @@ def medianFiltering(m,n,old,new,window_size):
 
 
 def main():
-    print(os.path.exists('samplev2.png'))    
+    loop = 20
+    duration = []
+    #print(os.path.exists('samplev2.png'))    
     #take input by cv2
-    img_input = cv2.imread('samplee.png', 1)
+    img_input = cv2.imread('sample.png', 1)
+    start_timer = int(time.time())
     m, n, c = img_input.shape
     matrix_size = 3
     pos= int(matrix_size/2)
-    print('input image shape', img_input.shape)
-    print('Input Image', img_input)
 
-    print('m= ', m)
-    print('n= ', n)
-    print('c= ', c)
+
+    # print('input image shape', img_input.shape)
+    # print('Input Image', img_input)
+
+    # print('m= ', m)
+    # print('n= ', n)
+    # print('c= ', c)
 
     updatedImage = np.array([[[0]*3]*(n+(matrix_size-1))]*(m+(matrix_size-1)))
     
-    print('updatedImage shape', updatedImage.shape)
-    print('updatedImage after declaring np array', updatedImage)
+    # print('updatedImage shape', updatedImage.shape)
+    # print('updatedImage after declaring np array', updatedImage)
 
     updatedImage[pos:m+pos , pos:n+pos, :] = img_input
 
@@ -83,26 +89,43 @@ def main():
     r_new = np.zeros([m+(matrix_size-1), n+(matrix_size-1)])
 
 
-    print('updatedImage after using img_input', updatedImage)
+    # print('updatedImage after using img_input', updatedImage)
     b, g, r = cv2.split(updatedImage)
 
 
 
-    print('Before sending: m,n',m,n)
+    # print('Before sending: m,n',m,n)
 
-    print('Before sending: b',b)
+    # print('Before sending: b',b)
 
-    print('Before sending: b_new',b_new)
+    # print('Before sending: b_new',b_new)
 
+    for iter in range(loop):
+        start_timer = int(time.time())
 
-    b_final= medianFiltering(m,n,b,b_new,matrix_size)
-    g_final= medianFiltering(m,n,g,g_new,matrix_size)
-    r_final= medianFiltering(m,n,r,r_new,matrix_size)
-
+        b_final= medianFiltering(m,n,b,b_new,matrix_size)
+        g_final= medianFiltering(m,n,g,g_new,matrix_size)
+        r_final= medianFiltering(m,n,r,r_new,matrix_size)
+        stop_timer = int(time.time())
+        
+        duration_in_second = stop_timer-start_timer
+        duration.append(duration_in_second)
     
     final_img = cv2.merge((b_final,g_final,r_final))
-    print('final_b ', b_final)
+    # print('final_b ', b_final)
     cv2.imwrite('filtered.png', final_img)
+
+
+
+    #csv part
+    filename = "test.csv"
+    data = pd.read_excel(filename)
+    data["day"] = duration
+    # Use writerows() not writerow()
+    # writer.writerows(report_header)
+    data.to_csv("test.csv")
+    
+    # print('###time', duration_in_second)
     # print('Updated Image to', final_img)
     # print('b_new should be', b_final)
     # print('g_new should be', g_final)
