2a1873486d5e450558a118e92f2dde790d6ed7f2
_nafiz <nafiz.hossain@tigerit.com>
2022-11-09 17:51:03 +0600

    Modified fiveByfive Matrix

--- medianFiltering.py
+++ medianFiltering.py
@@ -50,11 +50,13 @@ def medianFilteringThreeByThree(m,n,old,new):
 def medianFilteringFiveByFive(m,n,old,new):
     for i in range(2, m+2):
         for j in range(2, n+2):
-            temp = [ old[i-2, j-2],
+            temp = [ 
+            old[i-2, j-2],
             old[i-2, j-1],
             old[i-2, j],
             old[i-2, j+1],
             old[i-2, j+2],
+
             old[i-1, j-2],
             old[i-1, j-1],
             old[i-1, j],
@@ -71,6 +73,7 @@ def medianFilteringFiveByFive(m,n,old,new):
             old[i+1, j],
             old[i+1, j+1],
             old[i+1, j+2],
+
             old[i+2, j-2],
             old[i+2, j-1],
             old[i+2, j],
@@ -89,26 +92,26 @@ def medianFilteringFiveByFive(m,n,old,new):
     return new
 
 
-# def medianFilteringSevenBySeven(m,n,old,new):
-#     for i in range(1, m+1):
-#         for j in range(1, n+1):
-#             temp = [ old[i-1, j-1],
-#             old[i-1, j],
-#             old[i-1, j+1],
-#             old[i, j-1],
-#             old[i, j+1],
-#             old[i+1, j-1],
-#             old[i+1, j],
-#             old[i+1, j+1],
-#             ]
-#             temp = statistics.median(temp)
-#             new[i, j]= temp
+def medianFilteringSevenBySeven(m,n,old,new):
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            temp = [ old[i-1, j-1],
+            old[i-1, j],
+            old[i-1, j+1],
+            old[i, j-1],
+            old[i, j+1],
+            old[i+1, j-1],
+            old[i+1, j],
+            old[i+1, j+1],
+            ]
+            temp = statistics.median(temp)
+            new[i, j]= temp
 
-#     #astype returns a new DataFrame where the data types has been changed to the specified type
-#     #numpy.uint8: 8-bit unsigned integer (0 to 255).
-#     new = new.astype(np.uint8)
-#     final= new[1:m+1 , 1:n+1]
-#     return final
+    #astype returns a new DataFrame where the data types has been changed to the specified type
+    #numpy.uint8: 8-bit unsigned integer (0 to 255).
+    new = new.astype(np.uint8)
+    final= new[1:m+1 , 1:n+1]
+    return final
 
 
 
@@ -117,8 +120,8 @@ def main():
     #take input by cv2
     img_input = cv2.imread('samplee.png', 1)
     m, n, c = img_input.shape
-    matrix_size = 5
-
+    matrix_size = 7
+    pos= int(matrix_size/2)
     print('input image shape', img_input.shape)
     print('Input Image', img_input)
 
@@ -126,27 +129,17 @@ def main():
     print('n= ', n)
     print('c= ', c)
 
-    if (matrix_size==3):
-        updatedImage = np.array([[[0]*3]*(n+2)]*(m+2))
-    if (matrix_size==5):
-        updatedImage = np.array([[[0]*3]*(n+4)]*(m+4))
-    
+    updatedImage = np.array([[[0]*3]*(n+(matrix_size-1))]*(m+(matrix_size-1)))
     
     print('updatedImage shape', updatedImage.shape)
     print('updatedImage after declaring np array', updatedImage)
 
-    if (matrix_size==3):
-        updatedImage[1:m+1 , 1:n+1, :] = img_input
-        b_new = np.zeros([m+2, n+2])
-        g_new = np.zeros([m+2, n+2])
-        r_new = np.zeros([m+2, n+2])
+    updatedImage[pos:m+pos , pos:n+pos, :] = img_input
+    
+    b_new = np.zeros([m+(matrix_size-1), n+(matrix_size-1)])
+    g_new = np.zeros([m+(matrix_size-1), n+(matrix_size-1)])
+    r_new = np.zeros([m+(matrix_size-1), n+(matrix_size-1)])
 
-
-    if (matrix_size==5):
-        updatedImage[2:m+2 , 2:n+2, :] = img_input
-        b_new = np.zeros([m+4, n+4])
-        g_new = np.zeros([m+4, n+4])
-        r_new = np.zeros([m+4, n+4])
 
     print('updatedImage after using img_input', updatedImage)
     b, g, r = cv2.split(updatedImage)
@@ -166,6 +159,11 @@ def main():
         r_final= medianFilteringThreeByThree(m,n,r,r_new)
 
     if (matrix_size==5):
+        b_final= medianFilteringSevenBySeven(m,n,b,b_new)
+        g_final= medianFilteringSevenBySeven(m,n,g,g_new)
+        r_final= medianFilteringSevenBySeven(m,n,r,r_new)
+
+    if (matrix_size==7):
         b_final= medianFilteringFiveByFive(m,n,b,b_new)
         g_final= medianFilteringFiveByFive(m,n,g,g_new)
         r_final= medianFilteringFiveByFive(m,n,r,r_new)
