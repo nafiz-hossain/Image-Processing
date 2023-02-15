786f90b61eece69e0d92a121bd26722247880f7f
Nowrin Hossain <nowrinneetu95@gmail.com>
2023-02-15 09:36:06 +0600

    unusual

---
+++ fuzzy_noise_detector/util.py
@@ -0,0 +1,8 @@
+def mse(imageA, imageB):
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    return err
+
+def calculate_psnr(originalImage, filteredImage):
+    # return cv2.PSNR(originalImage,filteredImage)
+    return mse(originalImage, filteredImage)
